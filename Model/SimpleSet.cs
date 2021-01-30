aeaff29de6fe70cefe56c99c617df0bb168844ea
Malukov Nikita <3050838@mail.ru>
2021-01-30 22:54:20 +0300

    Добавьте файлы проекта.

---
+++ Model/SimpleSet.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Set.Model
+{
+    public class SimpleSet<T> : IEnumerable
+    {
+        private List<T> items = new List<T>();
+        public int Count => items.Count();
+        public SimpleSet()
+        {
+
+        }
+        public SimpleSet(IEnumerable<T> items)
+        {
+            this.items = items.ToList();
+        }
+
+        public SimpleSet(T data)
+        {
+            this.Add(data);
+        }
+        public void Add(T data)
+        {
+            if(items.Contains(data))
+            {
+                return;
+            }
+
+            items.Add(data);
+        }
+
+        public void Remove(T data)
+        {
+            items.Remove(data);
+        }
+
+        public SimpleSet<T> Union(SimpleSet<T> secondSet)
+        {
+            SimpleSet<T> simpleSet = new SimpleSet<T>();
+
+            foreach (var items in items)
+            {
+                simpleSet.Add(items);
+            }
+
+            foreach (var items in secondSet.items)
+            {
+                simpleSet.Add(items);
+            }
+
+            return simpleSet;
+        }
+
+        public SimpleSet<T> Intersection(SimpleSet<T> secondSet)
+        {
+            SimpleSet<T> newSet = new SimpleSet<T>();
+            List<T> bigger;
+            List<T> smaller;
+
+            if(this.Count >= secondSet.Count)
+            {
+                bigger = this.items;
+                smaller = secondSet.items;
+            }
+            else
+            {
+                bigger = secondSet.items;
+                smaller = this.items;
+            }
+
+            foreach (var item1 in smaller)
+            {
+                foreach (var item2 in bigger)
+                {
+                    if(item1.Equals(item2))
+                    {
+                        newSet.Add(item1);
+                        break;
+                    }
+                }
+            }
+
+            return newSet;
+        }
+
+        public SimpleSet<T> Difference(SimpleSet<T> secondSet)
+        {
+            SimpleSet<T> newSet = new SimpleSet<T>(items);
+
+            foreach (var item in secondSet.items)
+            {
+                newSet.Remove(item);
+            }
+
+            return newSet;
+        }
+
+        public bool Subset(SimpleSet<T> secondSet)
+        {
+            foreach (var item1 in items)
+            {
+                bool Equalse = false;
+                
+                foreach (var item2 in secondSet.items)
+                {
+                    if(item1.Equals(item2))
+                    {
+                        Equalse = true;
+                        break;
+                    }
+                }
+
+                if(Equalse == false)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public SimpleSet<T> SymmetricDifference(SimpleSet<T> secondSet)
+        {
+            SimpleSet<T> newSet = new SimpleSet<T>();
+
+            foreach (var item1 in this.items)
+            {
+                var Equals = false;
+                
+                foreach (var item2 in secondSet.items)
+                {
+                    if(item1.Equals(item2))
+                    {
+                        Equals = true;
+                        break;
+                    }
+                }
+
+                if(Equals == false)
+                {
+                    newSet.Add(item1);
+                }
+            }
+
+            foreach (var item1 in secondSet.items)
+            {
+                bool Equals = false;
+
+                foreach (var item2 in this.items)
+                {
+                    if (item1.Equals(item2))
+                    {
+                        Equals = true;
+                        break;
+                    }
+                }
+
+                if (Equals == false)
+                {
+                    newSet.Add(item1);
+                }
+            }
+
+            return newSet;
+        }
+        public IEnumerator GetEnumerator()
+        {
+            return items.GetEnumerator();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using Set.Model;
+using System;
+using System.Collections.Generic;
+
+namespace Set
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleSet<int> simpleSet1 = new SimpleSet<int>();
+            SimpleSet<int> simpleSet2 = new SimpleSet<int>();
+            SimpleSet<int> simpleSet3 = new SimpleSet<int>();
+
+            simpleSet1.Add(1);
+            simpleSet1.Add(2);
+            simpleSet1.Add(3);
+            simpleSet1.Add(4);
+            simpleSet1.Add(5);
+
+            simpleSet2.Add(4);
+            simpleSet2.Add(5);
+            simpleSet2.Add(6);
+            simpleSet2.Add(7);
+
+            simpleSet3.Add(1);
+            simpleSet3.Add(2);
+            simpleSet3.Add(3);
+
+            Console.WriteLine();
+            Console.WriteLine("Union: "); //1-2-3-5-4-5-6-7;
+            Console.WriteLine();
+
+            foreach (var item in simpleSet1.Union(simpleSet2))
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Difference: "); //1-2-3
+            Console.WriteLine();
+
+            foreach (var item in simpleSet1.Difference(simpleSet2))
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Intersection: "); //4-5
+            Console.WriteLine();
+
+            foreach (int item in simpleSet2.Intersection(simpleSet1))
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("SubSet: " + simpleSet3.Subset(simpleSet1)); //True;
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.WriteLine("SymmetricDifference: "); //1-2-3-6-7
+            Console.WriteLine();
+
+            foreach (int item in simpleSet1.SymmetricDifference(simpleSet2))
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
